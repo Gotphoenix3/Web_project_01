e88cec36115ce7ccab67d689856292028b93431d
Gotphoenix3 <mikemartinezch15@gmail.com>
2024-09-27 14:18:18 -0600

    New css added into the file

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -12,4 +12,22 @@ def logout():
 
 @auth.route('/sign-up')
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 characters.', category='error')
+        elif password1 != password2:
+            flash('Your passwords don´t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            # adding user to the database
+            flash('Account created', category='success')
+
     return render_template("sign_up.html")
